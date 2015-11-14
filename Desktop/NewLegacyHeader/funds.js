10a61ed98c8bc1d86f0fd9e2366f79ff6c05f7fe
Khalid <khalid.cheshire@yahoo.com>
2015-11-14 18:12:49 -0500

    Desktop/LegacyHeader

---
+++ Desktop/NewLegacyHeader/funds.js
@@ -0,0 +1,19 @@
+function hoverAlert(el, timeOut) {
+	$()
+}
+
+$('#bw').hover(function() {
+	console.log('hover');
+})
+
+$('#bp').hover(function() {
+	console.log('hover');
+})
+
+$('#bh').hover(function() {
+	console.log('hover');
+})
+
+$('#cx').hover(function() {
+	console.log('sleepy');
+})
